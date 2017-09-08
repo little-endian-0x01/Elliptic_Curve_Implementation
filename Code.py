dc05689be0830cc64ce28d34ad3e67bdf5f6a774
ConanKapoor <kapoor.shivam88@gmail.com>
2017-09-08 09:52:38 +0530

    Error fixed

--- Code.py
+++ Code.py
@@ -23,7 +23,8 @@ def Plot_Graph(a,b):
 
 # Generating points on Elliptic Curve
 def Point_Generation(a,b,prime):
-    print("Following points are on the Elliptical Curve : \n")
+    print("\n-----------------------------------------------\n")
+    print("\nFollowing points are on the Elliptical Curve : \n")
     for x in range(1, prime):
         for y in range(1, prime):
             if (y ** 2) % prime == (x**3 + (a * x) + b) % prime:
@@ -39,10 +40,10 @@ def inverse(prime, num):
 
 # Function to add to points on Ellipic Curve
 def Algebraic_Addition(x1, y1, x2, y2, a, prime):
-    if x1 == x2 and y1 == y2:
-        lamdba_value = ((3 * (x1 ** 2) + a) * inverse(prime, 2 * y1)) % prime
+    if (x1 == x2) and (y1 == y2):
+        lamdba_value = (((3 * (x1 ** 2)) + a) * (inverse(prime, 2 * y1))) % prime
     else:
-        lamdba_value = ((y2 - y1) * inverse(prime, x2 - x1)) % prime
+        lamdba_value = ((y2 - y1) * (inverse(prime, x2 - x1))) % prime
     x3 = (lamdba_value ** 2 - x2 - x1) % prime
     y3 = (lamdba_value * (x1 - x3) - y1) % prime
     return x3, y3
@@ -55,14 +56,13 @@ def Base_Point(a,prime):
         TempArray.append(Points_x[Temp])
         x1, y1 = Points_x[Temp], Points_y[Temp]
         for i in range(1, Points_x.__len__()):
-            print(i, Temp)
-            print(" (" + str(x1) + "," + str(y1) + ") ")
+            print(" => (" + str(x1) + "," + str(y1) + ") ")
             x1, y1 = Algebraic_Addition(x1, y1, x1, y1, a, prime)
             TempArray.append(x1)
 
         if set(Points_x).intersection(set(TempArray)) != 0:
-            Global_x, Global_y = x, y
-            print(" => (" + str(x) + "," + str(y) + ") ")
+            Global_x, Global_y = Points_x[Temp], Points_y[Temp]
+            print(" => (" + str(Global_x) + "," + str(Global_y) + ") ")
             break
         else:
             Temp += 1
@@ -70,32 +70,33 @@ def Base_Point(a,prime):
 
 # Key Generation Process
 def Key_Generation(a,prime):
-    print("\nKey Generation Process starts here : \n\n")
+    print("\n-------------------------------------\n")
+    print("Key Generation Process starts here : \n")
 
     # Private Key Generation
-    privateKey = randint(2, p - 1)
-    print("\nThe Random Number Chosen(The Private Key) is %d. ", privateKey)
+    privateKey = randint(2, prime - 1)
+    print("The Random Number Chosen, i.e.the Private Key is: ", privateKey)
 
     # Public Key Generation
     x1, y1 = Global_x, Global_y
     for i in range(0, privateKey):
-        x1, y1 = addPoints(x1, y1, x1, y1, a, prime)
+        x1, y1 = Algebraic_Addition(x1, y1, Global_x, Global_y, a, prime)
     PublicKey_x, PublicKey_y = x1, y1
 
     # Random value 'k' Generation
-    k = randint(2, p - 1)
+    k = randint(2, prime - 1)
     print("\nThe Random value k is %s ", k)
 
     # Generating C1 value
     print("The Cipher C1 is: ")
     for i in range(0, k):
-        x1, y1 = addPoints(Global_x, Global_y, Global_x, Global_y, a, prime)
+        x1, y1 = Algebraic_Addition(Global_x, Global_y, Global_x, Global_y, a, prime)
     print(" (" + str(x1) + "," + str(y1) + ") ")
 
     # Generating C1 value
     print("The Cipher C2 is: ")
     for i in range(0, k):
-        x1, y1 = addPoints(PublicKey_x, PublicKey_y, PublicKey_x, PublicKey_y, a, prime)
+        x1, y1 = Algebraic_Addition(PublicKey_x, PublicKey_y, PublicKey_x, PublicKey_y, a, prime)
         return x1,y1
 
 # Generating Message coordinates (Here ASCII conversion)
@@ -106,7 +107,7 @@ def Message_Generation(message):
 
 # Generating Cipher Text
 def Generation_CipherText(x1,y1,a,prime):
-    cipher_x, cipher_y = addPoints(x, y, x1, y1, a, prime)
+    cipher_x, cipher_y = Algebraic_Addition(x, y, x1, y1, a, prime)
     print(" (" + str(cipher_x) + "," + str(cipher_y) + ") ")
 
 ###########################################################################################################
@@ -121,7 +122,7 @@ print("\n\t\t y^2 = x^3 + a*x + b\n")
 def Input_Data():
     prime = int(input("Please Enter a prime number:  "))
     a = int(input("Please Enter the value of a:  "))
-    b = int(input("Please Enter the value of b: "))
+    b = int(input("Please Enter the value of b:  "))
     message = input("Please Enter the data to be send(Single letter between a-z) : ")
     if (((4 * (a ** 3)) + (27 * (b ** 2))) % prime) == 0:
         print("a and b values don't satisfy basic condition.\n")
@@ -131,7 +132,7 @@ def Input_Data():
         return a, b, prime, message
 
 a, b, prime, message = Input_Data()
-Plot_Graph(a,b)
+# Plot_Graph(a,b)
 Point_Generation(a,b,prime)
 Base_Point(a,prime)
 x1,y1 = Key_Generation(a,prime)
